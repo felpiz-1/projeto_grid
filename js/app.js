5c9893344ba1a408af07ebf5fac86470d1a010ae
FelipeRocha <rochadeandradef@gmail.com>
2026-08-23 12:33:12 -0300

    funçaõ carrega as mensagens do contato clicado

--- js/app.js
+++ js/app.js
@@ -20,12 +20,42 @@ fotoPerfilNav.addEventListener('click', abrirPerfil);
 iconeMensagensNav.addEventListener('click', abrirMensagens);
 
 const elemento = {
-    listaContatos: document.querySelector(".container-mensagens")
+    listaContatos: document.querySelector(".container-mensagens"),
+    gridMensagens: document.querySelector(".grid-msg"),
+    nomeTopo: document.querySelector(".container-topo-mensagens h2"),
+    fotoTopo: document.querySelector(".container-topo-mensagens .foto-perfil")
+};
+
+function carregarMensagens(contato, fotoUrl) {
+    elemento.nomeTopo.innerText = contato.name;
+    elemento.fotoTopo.src = fotoUrl;
+    elemento.gridMensagens.innerHTML = "";
+
+    contato.messages.forEach((msg) => {
+        const divMsg = document.createElement('div');
+        const divSeta = document.createElement('div');
+        const pTexto = document.createElement('p');
+        const spanHora = document.createElement('span');
+
+        const ehMinhaMsg = msg.sender === "me";
+
+        if (ehMinhaMsg) {
+            divMsg.className = "msg-enviada";
+            divSeta.className = "affs-enviadas";
+        } else {
+            divMsg.className = "msg-recebida";
+            divSeta.className = "affs";
+        }
+
+        pTexto.innerText = msg.content;
+        spanHora.innerText = msg.time;
+
+        divMsg.append(divSeta, pTexto, spanHora);
+        elemento.gridMensagens.appendChild(divMsg);
+    });
 }
 
-
-
-function criarontatos(srcFoto, nome, hora, ultima, naoLidas, idContato) {
+function criarontatos(srcFoto, nome, hora, ultima, naoLidas, idContato, aoClicar) {
     const cardContatos = document.createElement('div');
     const fotoContato = document.createElement('img');
     const nomeContato = document.createElement('p');
@@ -47,9 +77,10 @@ function criarontatos(srcFoto, nome, hora, ultima, naoLidas, idContato) {
     horaMsg.innerText = hora;
     previaMensagem.innerText = ultima;
     msgNaoLidas.innerText = naoLidas;
-    
     cardContatos.id = idContato;
     
+    cardContatos.addEventListener('click', aoClicar);
+
     containerNotificacao.appendChild(msgNaoLidas);
     cardContatos.append(fotoContato, nomeContato, horaMsg, previaMensagem, containerNotificacao);
     
@@ -70,7 +101,13 @@ function carregarContatos() {
 
         const fotoUrl = `https://i.pravatar.cc/150?img=${index + 12}`;
 
-        criarontatos(fotoUrl,contato.name,horaUltimaMsg,textoUltimaMsg,"1",`contato-${index}`);
+        const aoClicar = () => carregarMensagens(contato, fotoUrl);
+
+        if (index === 0) {
+            carregarMensagens(contato, fotoUrl);
+        }
+
+        criarontatos(fotoUrl, contato.name, horaUltimaMsg, textoUltimaMsg, "1", `contato-${index}`, aoClicar);
     });
 }
 
